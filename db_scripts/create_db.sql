4cbe0ec2d55d6a8dcb445518977e89f41a8ede9b
ochetcuti <chetcuti.oliver@gmail.com>
2024-10-09 20:25:47 +0100

    hidden spaces between 10 and 17 causing mysql error on lower lines

--- db_scripts/create_db.sql
+++ db_scripts/create_db.sql
@@ -8,9 +8,9 @@ USE bettys_books;
 CREATE TABLE IF NOT EXISTS books (id INT AUTO_INCREMENT,name VARCHAR(50),price DECIMAL(5, 2) unsigned,PRIMARY KEY(id));
 
 CREATE TABLE IF NOT EXISTS users(
-	ID VARCHAR(36) DEFAULT (uuid()),
+    ID VARCHAR(36) DEFAULT (uuid()),
     UserName varchar(225) NOT NULL,
-	LastName varchar(255) NOT NULL,
+    LastName varchar(255) NOT NULL,
     FirstName varchar(255) NOT NULL,
     Email varchar(255) NOT NULL,
     Password BINARY(60) NOT NULL,
